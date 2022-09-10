f3bd36dd2619c01becbc1eb19269c9555d154db4
rivaslive <krivas@brandy.la>
2022-09-10 11:31:30 -0600

    Fixed sort website

--- website/src/components/Templates/AllComponentTemplate/AllComponent.tsx
+++ website/src/components/Templates/AllComponentTemplate/AllComponent.tsx
@@ -16,6 +16,16 @@ import { defaultName } from 'utils/defaultVersion';
 const AllComponentTemplate = () => {
   const { query } = useRouter();
 
+  const sortComponents = (a: { name: string }, b: { name: string }) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  };
+
   return (
     <>
       <Seo title="components" />
@@ -36,34 +46,24 @@ const AllComponentTemplate = () => {
           ]}
         >
           {v1.map(({ children }) => {
-            return children
-              .sort((a, b) => {
-                if (a.name < b.name) {
-                  return -1;
-                }
-                if (a.name > b.name) {
-                  return 1;
-                }
-                return 0;
-              })
-              .map(({ name, key, icon }) => {
-                let path;
-                if (key === 'getting-started') {
-                  path = `/${query?.version as string}/`;
-                } else {
-                  path = `/${query?.version as string}/components/`;
-                }
+            return children.sort(sortComponents).map(({ name, key, icon }) => {
+              let path;
+              if (key === 'getting-started') {
+                path = `/${query?.version as string}/`;
+              } else {
+                path = `/${query?.version as string}/components/`;
+              }
 
-                return (
-                  <Col key={key} xs={24} md={12} xxl={8}>
-                    <Link href={path + key} passHref>
-                      <a>
-                        <CardComponentList title={name} icon={icon} />
-                      </a>
-                    </Link>
-                  </Col>
-                );
-              });
+              return (
+                <Col key={key} xs={24} md={12} xxl={8}>
+                  <Link href={path + key} passHref>
+                    <a>
+                      <CardComponentList title={name} icon={icon} />
+                    </a>
+                  </Link>
+                </Col>
+              );
+            });
           })}
         </Row>
       </Layout>
